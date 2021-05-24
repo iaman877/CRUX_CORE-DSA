d7effc823720c41c06ddf7c65258e7728f78ec06
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-05-24 10:20:08 +0530

    Create Z.java

---
+++ Free_Resources_by_PepCoding/Level-1/Basics of Programming/Getting Started/Z.java
@@ -0,0 +1,11 @@
+// You are required to print a 'z' of size 5 using '*'.
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*****");
+        System.out.println("   * ");
+        System.out.println("  *  ");
+        System.out.println(" *   ");
+        System.out.println("*****");
+    }
+}
