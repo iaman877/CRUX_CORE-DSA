e1e28c177c14b4bca9e6549e183a6b7e9ad67632
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-06-18 23:41:38 +0530

    Create GCD.java

---
+++ Crux_DSA/Daily Practice/18th june64/GCD.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int n1 = scn.nextInt();
+    int n2 = scn.nextInt();
+    while(n1 % n2 != 0)
+       {
+           int rem = n1 % n2;
+           n1 = n2;
+           n2 = rem;
+       }
+       System.out.println(n2);
+    }
+}
