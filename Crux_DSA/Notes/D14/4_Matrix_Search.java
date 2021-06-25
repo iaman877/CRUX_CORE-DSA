d0ea62d88654b57c4d18c9e2b1f7a287b3540333
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-06-25 21:17:12 +0530

    Create 4_Matrix_Search.java

---
+++ Crux_DSA/Notes/D14/4_Matrix_Search.java
@@ -0,0 +1,23 @@
+// leetcode meduim
+public class Matrix_Search {
+	public static void main(String[] args) {
+		int[][] arr = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 } };
+		System.out.println(Search(arr, 34));
+	}
+	public static int Search(int [][] arr, int target) {
+		int r =0;
+		int c = arr[0].length-1;
+		while(r<arr.length && c>=0) {
+			if(arr[r][c]==target) {
+				return 1;
+			}
+			if(arr[r][c]>target) {
+				c--;
+			}
+			else {
+				r++;
+			}
+		}
+		return 0;	
+	}
+}
