4da61fe267873bc87ddac245f7ae12014529bf67
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-07-07 11:31:58 +0530

    Create EX.sql

---
+++ SQL/w3schools/5-SQL NULL/EX.sql
@@ -0,0 +1,5 @@
+-- Select all records from the Customers where the PostalCode column is empty
+SELECT * FROM Customers WHERE PostalCode IS NOT NULL;
+
+-- Select all records from the Customers where the PostalCode column is NOT empty.
+SELECT * FROM Customers WHERE  PostalCode IS NOT NULL;
