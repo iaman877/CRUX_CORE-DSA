248b9b2f71d324ea9c87f63ffec8dc30d354d818
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-07-09 12:22:17 +0530

    v2

--- Free_Resources_by_PepCoding/Level-1/Recursion and Backtracking/Recursion in Array/MaxArray.java
+++ Free_Resources_by_PepCoding/Level-1/Recursion and Backtracking/Recursion in Array/MaxArray.java
@@ -1,36 +1,6 @@
 import java.io.*;
 import java.util.*;
-
 public class Main {
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int arr[] = new int[n];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        int max = maxOfArray(arr, 0);
-        System.out.println(max);
-    }
-
-    public static int maxOfArray(int[] arr, int idx) {
-        if (idx == arr.length) {
-            return Integer.MIN_VALUE;
-        }
-        int max = maxOfArray(arr, idx + 1);
-        if (arr[idx] > max) {
-            max = arr[idx];
-        }
-        return max;
-    }
-}
--------------
-    import java.io.*;
-import java.util.*;
-
-public class Main {
-
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n =  scn.nextInt();
@@ -47,7 +17,7 @@ public class Main {
             return arr[idx];
         }
         int max = maxOfArray(arr, idx + 1);
-        if (arr[idx] > max) {
+        if (max > arr[idx]) {
              return max;
         }
         else {
