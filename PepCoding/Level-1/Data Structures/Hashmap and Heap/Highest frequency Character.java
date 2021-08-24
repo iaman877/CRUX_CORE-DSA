7f1adf13b39d5323379fd0a4b0660dcf16d03ec3
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2021-08-24 08:08:36 +0530

    Update Highest frequency Character.java

--- PepCoding/Level-1/Data Structures/Hashmap and Heap/Highest frequency Character.java
+++ PepCoding/Level-1/Data Structures/Hashmap and Heap/Highest frequency Character.java
@@ -1,3 +1,18 @@
+
+/*
+1. You are given a string str.
+2. You are required to find the character with maximum frequency.
+Input Format
+A string str
+Output Format
+The character with highest frequency
+
+Sample Input
+zmszeqxllzvheqwrofgcuntypejcxovtaqbnqyqlmrwitc
+Sample Output
+q
+
+*/
 import java.io.*;
 import java.util.*;
 public class Main{
