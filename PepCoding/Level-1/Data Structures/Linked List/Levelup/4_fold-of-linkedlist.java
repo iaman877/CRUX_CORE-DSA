01847ddb90a5f4f170414e13eb6e4278cf6f1c4d
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-01-16 22:56:01 +0530

    Initial commit

---
+++ PepCoding/Level-1/Data Structures/Linked List/Levelup/4_fold-of-linkedlist.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+class Main {
+    public static class ListNode {
+        int val = 0;
+        ListNode next = null;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+    
+    public static ListNode midNode(ListNode head)
+    {
+        if(head == null || head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode reverseLL(ListNode head)
+    {
+        if(head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode forw = null;
+        while(curr != null)
+        {
+            forw = curr.next;  // baclup
+            curr.next = prev;   // Link
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+    public static void fold(ListNode head) {
+        if(head == null || head.next == null) return;
+        ListNode mid = midNode(head);
+        ListNode nHead = mid.next;
+        mid.next = null;
+        nHead = reverseLL(nHead);
+        ListNode c1 = head;
+        ListNode c2 = nHead;
+        ListNode f1 = null;
+        ListNode f2 = null;
+        
+        while(c2!=null)
+        {
+            // backup
+            f1 = c1.next;
+            f2 = c2.next;
+            
+            // link
+            c1.next =c2;
+            c2.next = f1;
+            
+            // move
+            c1 = f1;
+            c2 = f2;
+        }
+    }
+
+    static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        ListNode dummy = new ListNode(-1);
+        ListNode prev = dummy;
+        while (n-- > 0) {
+            prev.next = new ListNode(scn.nextInt());
+            prev = prev.next;
+        }
+
+        ListNode head = dummy.next;
+        fold(head);
+        printList(head);
+    }
+}
