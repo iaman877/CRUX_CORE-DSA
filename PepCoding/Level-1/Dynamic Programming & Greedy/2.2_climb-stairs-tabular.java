e05a156ca9c9274a6086164e58498effd8089607
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-01-27 23:49:30 +0530

    Initial commit

---
+++ PepCoding/Level-1/Dynamic Programming & Greedy/2.2_climb-stairs-tabular.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int cp = countPathsTab(n);
+        System.out.println(cp);
+    }
+    public static int countPathsTab(int n)
+    {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        for(int i =1;i<=n;i++)
+        {
+            if(i == 1)
+             {
+                dp[i] = dp[i-1];
+             }
+             else if(i ==2)
+             {
+                dp[i] = dp[i-1] + dp[i -2];
+             }
+             else
+             {
+                dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+             }
+        }
+        return dp[n];
+    }
+}
