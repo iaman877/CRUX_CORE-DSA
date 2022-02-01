d9e2a1b7c4b3d720a61ca76c92816a82fb4cb644
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-02-02 00:00:17 +0530

    update

--- PepCoding/Level-1/Data Structures/Stacks/4_stock-span.java
+++ PepCoding/Level-1/Data Structures/Stacks/4_stock-span.java
@@ -30,16 +30,18 @@ public static void main(String[] args) throws Exception {
     st.push(0);
     span[0] = 1;
 
-    for (int i = 1; i < arr.length; i++) {
-      while (st.size() > 0 && arr[i] >= arr[st.peek()]) {
+    for (int i = 1; i < arr.length; i++)
+    {
+      while (st.size() > 0 && arr[i] >= arr[st.peek()]) 
+      {
         st.pop();
       }
-      if (st.size() == 0) {
+      if (st.size() == 0)
+      {
         span[i] = i + 1;
       } else {
         span[i] = i - st.peek();
       }
-
       st.push(i);
     }
     return span;
