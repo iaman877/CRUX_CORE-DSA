bdb0c58ba8663ccf70b7cbedeedb0ddd734911fc
Aman Bhardwaj <49730521+iaman877@users.noreply.github.com>
2022-06-30 08:18:27 +0530

    added method 2

--- PepCoding/Level-1/Basics/Getting Started/Count Digits In A Number.java
+++ PepCoding/Level-1/Basics/Getting Started/Count Digits In A Number.java
@@ -3,6 +3,9 @@
 2. Take as input "n", the number for which the digits has to be counted.
 3. Print the digits in that number.
 */
+
+// Solution 1st 
+
 import java.util.*;  
   public class Main{
   
@@ -18,5 +21,21 @@ import java.util.*;
    }
                                 
     System.out.println(count);
+   }
+  }
+// Solution 2nd
+import java.util.*;
+  public class Main
+  {
+    public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    long n = scn.nextInt();
+    String s=String.valueOf(n);
+    long counter = 0;
+    for(long i =0;i<s.length();i++)
+    {
+        counter++;
+    }
+    System.out.println(counter);
    }
   }
